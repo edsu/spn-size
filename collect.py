8e66c106dd4a562551245dcaefc877649876172c
Ed Summers <ehs@pobox.com>
2021-10-03 22:09:43 +0000

    abstract so that it can be run against any collection id so we can compare save-page-now with liveweb

--- collect.py
+++ collect.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+# usage: collect.py <collection-id>
 
 import re
 import csv
+import sys
 import internetarchive as ia
-
-# TODO: make this script smart enough to not collect data already in spn2-size.csv
 
 def item_summary(item_id):
     item = ia.get_item(item_id)
@@ -17,20 +18,25 @@ def item_summary(item_id):
         if file['name'].endswith('arc.gz'):
             size += int(file['size'])
 
-    m = re.match('^spn2-(\d\d\d\d)(\d\d)(\d\d)', item.item_metadata['metadata']['identifier'])
+    m = re.match('^.+-(\d\d\d\d)(\d\d)(\d\d)', item.item_metadata['metadata']['identifier'])
     date = '%s-%s-%s' % m.groups()
 
     return date, size
 
+if len(sys.argv) != 2:
+    sys.exit("usage: collect.py <collection-id>")
+
+collection_id = sys.argv[1]
+
 sizes = {}
-for result in ia.search_items('collection:save-page-now'):
+for result in ia.search_items(f'collection:{collection_id}'):
     date, size = item_summary(result['identifier'])
     if date and size: 
         sizes[date] = sizes.get(date, 0) + size
         print(date, size, sizes[date])
 
 dates = sorted(sizes.keys())
-writer = csv.writer(open('data.csv', 'w'))
+writer = csv.writer(open(f'{collection-id}.csv', 'w'))
 writer.writerow(['date', 'bytes'])
 
 for date in dates:
